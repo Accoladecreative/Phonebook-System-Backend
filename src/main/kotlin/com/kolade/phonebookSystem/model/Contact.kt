55d7e86c95580ecbb3e1ffa0ff215beb95077f78
Kolade Oluwadare <kolade4jesus10@gmail.com>
2023-11-08 02:11:34 +0100

    created a contact data class

--- src/main/kotlin/com/kolade/phonebookSystem/model/Contact.kt
+++ src/main/kotlin/com/kolade/phonebookSystem/model/Contact.kt
@@ -1,3 +1,8 @@
 package com.kolade.phonebookSystem.model
 
-data class Contact()
+
+data class Contact(val name: String, val phoneNumber: String, val address: String) {
+    override fun toString(): String {
+        return "Name: $name, Phone Number: $phoneNumber, Address: $address"
+    }
+}
