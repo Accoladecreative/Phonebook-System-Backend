7493ac7ae02e7eae8a7a8e03c170dbef733aed98
Kolade Oluwadare <kolade4jesus10@gmail.com>
2023-11-08 10:00:53 +0100

    updated  contact data class, adding id, implemented contact repo and repo class

--- src/main/kotlin/com/kolade/phonebookSystem/service/ContactService.kt
+++ src/main/kotlin/com/kolade/phonebookSystem/service/ContactService.kt
@@ -7,7 +7,7 @@ import com.kolade.phonebookSystem.model.Contact
 interface ContactService {
 
     fun createContact(contact: Contact?): ApiResponse?
-        fun getContact(phoneNUmber: String?): ApiResponse?
+    fun getContact(phoneNUmber: String?): ApiResponse?
     fun getALlContacts(allContacts: List<Contact>?): ApiResponse?
     fun updateContact(phoneNUmber: String?): ApiResponse?
     fun deleteContact(phoneNUmber: String?): ApiResponse?
